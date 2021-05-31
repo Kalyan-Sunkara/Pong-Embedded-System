ae9200d790b29e9a9b7964464d1c9c2108e8313a
Kalyan Sunkara <63612268+Kalyan-Sunkara@users.noreply.github.com>
2021-05-30 21:18:24 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -10,14 +10,119 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include <avr/interrupt.h>
 #endif
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn(){
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
+}
+
+void TimerOff(){
+    TCCR1B = 0x00;
+}
+void TimerISR(){
+    TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+
+}
+
+void TimerSet(unsigned long M){
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
+typedef struct task{
+    signed char state;
+    unsigned long int period;
+    unsigned long int elapsedTime;
+    int (*TickFct)(int);
+} task;
+
+enum Demo_States {shift};
+int Demo_Tick(int state) {
+
+	// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFE;  	// Row(s) displaying pattern. 
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift:	
+break;
+		default:	
+state = shift;
+			break;
+	}	
+	// Actions
+	switch (state) {
+case shift:	
+if (row == 0xEF && pattern == 0x01) { // Reset demo 
+				pattern = 0x80;		    
+				row = 0xFE;
+			} else if (pattern == 0x01) { // Move LED to start of next row
+				pattern = 0x80;
+				row = (row << 1) | 0x01;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		default:
+	break;
+	}
+	PORTC = pattern;	// Pattern to display
+	PORTD = row;		// Row(s) displaying pattern	
+	return state;
+}
+
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
+    static task task1;
+    
+    task *tasks[] = {&task1, &task2, &task3};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+    const char start = 0;
+    
+    task1.state = start;
+    task1.period = 100;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &Demo_States;
+    
+    TimerSet(1);
+    TimerOn();
+    
+    unsigned short x;
     while (1) {
-
+          for(x = 0; x < numTasks; x++){
+		    if(tasks[x]->elapsedTime == tasks[x]->period){
+			    tasks[x]->state = tasks[x]->TickFct(tasks[x]->state);
+			    tasks[x]->elapsedTime = 0;
+		    }
+		tasks[x]->elapsedTime += 1;
+	    }
+	while(!TimerFlag);
+	TimerFlag = 0;
     }
     return 1;
 }
